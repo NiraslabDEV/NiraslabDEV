d868aeae021028c282f10ee08e96887161567f1d
Niraslab DEV <niraslab.dev@gmail.com>
2025-02-07 02:19:01 -0300

    Atualizadas as menssagem do sistema

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ import { turboLogger } from "./turboLogger.js";
 
 console.log("Estou usando o github");
 
-turboLogger("Estou aprendendo o fluxo do git", "yellon");
+turboLogger("Estou aprendendo o fluxo do git", "red");
+turboLogger(
+  "Eu consegui criar uma menssagem sem afetar a versão principal do sistema",
+  "darkgreen"
+);
